baceb712182cbfcec5d8a64f142b7640d40ad30b
Huy Pham N.A <huy.pham@asiantech.vn>
2017-11-21 15:54:55 +0700

    first commit

---
+++ app/src/main/java/com/huy/learngit/MainActivity.java
@@ -0,0 +1,17 @@
+package com.huy.learngit;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by huyphamna on 21/11/2017.
+ */
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
